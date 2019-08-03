522b6c2cc2ec22f41ee1d6589acca928d73fbec6
Palash Sarkar <palashsarkar0007@gmail.com>
2019-08-03 18:13:34 +0530

    Comment modifications !!

--- src/main/java/org/dld/datahandling/Dbops.java
+++ src/main/java/org/dld/datahandling/Dbops.java
@@ -1,9 +1,14 @@
-/*
-
-        @Author : Tejas07PSK (Palash Sarkar),
-        @CreatedON : 30 Jul, 2019, 9:20 AM,
-        @File-Name : Dbops.java
-
+/*  -> Designed for testing and development purposes.
+ *  -> Project to design a 'SecurityPatching' prototype.
+ *  -> Development Phase -- Intermediate.
+ *  -> Project Type -- Educational.
+ *  -> Owner/Designer of code file :
+ *             @ Name - Palash Sarkar.
+ *             @ Email - palashsarkar0007@gmail.com.
+ *  -> Copyright Norms - Every piece of code given below has been written by 'Palash Sarkar (Tj07)'©,
+ *                       and he holds the rights to the file. Not meant to be
+ *                       copied or tampered with, without prior permission from the author.
+ *  -> Guide - Rudraprasad Pradhan.
  */
 
 package org.dld.datahandling;
--- src/main/java/org/dld/mailing/AES.java
+++ src/main/java/org/dld/mailing/AES.java
@@ -8,7 +8,7 @@
  *  -> Copyright Norms - Every piece of code given below has been written by 'Palash Sarkar (Tj07)'©,
  *                       and he holds the rights to the file. Not meant to be
  *                       copied or tampered with, without prior permission from the author.
- *  -> Guide - Balaji Chinthakalaya.
+ *  -> Guide - Rudraprasad Pradhan.
  */
 
 package org.dld.mailing;
